34c95ef9696629c093cb693a10242b16b66616ad
Koldo Pina <koldopinaortiz@gmail.com>
2018-02-06 21:20:22 +0100

    adding load default connection

--- core.py
+++ core.py
@@ -12,3 +12,11 @@ from tools import usedComPorts, stepTH
 
 #We check the COM port that we are using
 print (usedComPorts())
+
+#Load connection default parameters
+method = stepTH()['default_config']['method']
+stopbits = stepTH()['default_config']['stopbits']
+bytesize = stepTH()['default_config']['bytesize']
+parity = stepTH()['default_config']['parity']
+baudrate = stepTH()['default_config']['baudrate']
+timeout = stepTH()['default_config']['timeout']
